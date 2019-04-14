778038f36b2add0ce525d5d5675f8580bdfb85ae
Anik <anik7303@gmail.com>
2019-04-14 11:12:33 +0600

    show received snaps

---
+++ app/src/main/java/com/anikmohammad/snapchatclone/SnapViewActivity.java
@@ -0,0 +1,13 @@
+package com.anikmohammad.snapchatclone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SnapViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snap_view);
+    }
+}
--- app/src/main/java/com/anikmohammad/snapchatclone/SnapsActivity.java
+++ app/src/main/java/com/anikmohammad/snapchatclone/SnapsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,11 +12,16 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.StorageReference;
@@ -23,11 +29,12 @@ import com.google.firebase.storage.StorageReference;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class SnapsActivity extends AppCompatActivity {
+public class SnapsActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private FirebaseAuth mAuth;
     private ListView snapsListView;
     private ArrayList<HashMap<String, String>> snapsList;
+    private ArrayList<String> snapImageUrls;
     private SimpleAdapter adapter;
 
     private StorageReference storageReference;
@@ -46,7 +53,7 @@ public class SnapsActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         snapsListView = findViewById(R.id.snapsListView);
         snapsList = new ArrayList<>();
-        adapter = new SimpleAdapter(SnapsActivity.this, snapsList, android.R.layout.simple_list_item_2, new String[]{"", ""}, new int[] {, });
+        adapter = new SimpleAdapter(SnapsActivity.this, snapsList, android.R.layout.simple_list_item_2, new String[]{"message", "senderEmail"}, new int[] {android.R.id.text1, android.R.id.text2});
         snapsListView.setAdapter(adapter);
 
         populateListView();
@@ -78,7 +85,77 @@ public class SnapsActivity extends AppCompatActivity {
     }
 
     private void populateListView() {
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("users");
+        snapsList.clear();
+//        snapImageUrls.clear();
+        Log.i("List view population", "running");
+        FirebaseDatabase.getInstance().getReference()
+                .child("users")
+                .addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        final String userId = dataSnapshot.getKey();
+                        Log.i("currentUserID", FirebaseAuth.getInstance().getUid());
+                        if(dataSnapshot.getKey().equals(FirebaseAuth.getInstance().getUid())) {
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child("users")
+                                    .child(userId)
+                                    .child("snaps")
+                                    .addChildEventListener(new ChildEventListener() {
+                                        @Override
+                                        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                                            final String snapId = dataSnapshot.getKey();
+                                            HashMap<String, String> temp = new HashMap<>();
+                                            temp.put("senderEmail", dataSnapshot.child("senderEmail").getValue().toString());
+                                            temp.put("message", dataSnapshot.child("message").getValue().toString());
+                                            temp.put("imageUrl", dataSnapshot.child("imageUrl").getValue().toString());
+                                            temp.put("snapId", dataSnapshot.getKey());
+                                            snapsList.add(temp);
+                                            adapter.notifyDataSetChanged();
+                                        }
+
+                                        @Override
+                                        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                                        }
+
+                                        @Override
+                                        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                                        }
+
+                                        @Override
+                                        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                                        }
+                                    });
+                        }
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {}
+                });
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if(parent.getId() == R.id.snapsListView) {
+            Intent intent = new Intent(SnapsActivity.this, SnapViewActivity.class);
+            HashMap<String, String> temp = snapsList.get(position);
+            intent.putExtra("senderUid", temp.get("senderId"));
+            intent.putExtra("senderEmail", temp.get("senderEmail"));
+            intent.putExtra("imageUrl", temp.get("imageUrl"));
+            startActivity(intent);
+        }
     }
 
     private void redirectToCreateSnap() {
